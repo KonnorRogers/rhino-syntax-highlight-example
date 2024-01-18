e947bed5aa39920d38b975d3bdcd5919823117b1
Konnor Rogers <konnor5456@gmail.com>
2024-01-18 02:35:11 -0500

    update form stuff

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -37,12 +37,16 @@ function extendRhinoEditor (event) {
 
 document.addEventListener("rhino-initialize", extendRhinoEditor)
 
+// This next part is specifically for storing fully syntax highlighted markup in your database.
+//
+// On form submission, it will rewrite the value of the
 const highlightCodeblocks = (content) => {
   const doc = new DOMParser().parseFromString(content, 'text/html');
-  doc.querySelectorAll('pre > code:not(.hljs)').forEach((el) => {
+  // If it has the "[has-highlighted]" attribute attached, we know it has already been syntax highlighted.
+  // This will get stripped from the editor.
+  doc.querySelectorAll('pre > code[has-highlighted]').forEach((el) => {
     const html = toHtml(lowlight.highlightAuto(el.innerHTML).children)
-    // Add a `"hljs"` class
-    el.classList.add("hljs")
+    el.setAttribute("has-highlighted", "")
     el.innerHTML = html
   });
   let finalStr = doc.body.innerHTML;
@@ -50,11 +54,11 @@ const highlightCodeblocks = (content) => {
   return finalStr
 };
 
-
 document.addEventListener("submit", (e) => {
-  const inputElement = document.getElementById(e.target.querySelector("rhino-editor").getAttribute("input"))
+  // find all rhino-editor inputs attached to this form and transform them.
+  const rhinoInputs = [...e.target.elements].filter((el) => el.classList.contains("rhino-editor-input"))
 
-  if (!inputElement) return
-
-  inputElement.value = highlightCodeblocks(inputElement.value)
+  rhinoInputs.forEach((inputElement) => {
+    inputElement.value = highlightCodeblocks(inputElement.value)
+  })
 })
